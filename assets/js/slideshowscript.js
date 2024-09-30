14c8014a1d3bd4b70a9cef944f8ae2d05d6e7e67
TheTechRocket <51212745+TheTechRocket@users.noreply.github.com>
2024-09-29 23:46:13 -0400

    Changing images, adjusting folder structure for images, other small tweeks

--- assets/js/slideshowscript.js
+++ assets/js/slideshowscript.js
@@ -1,15 +1,3 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let slides = document.getElementsByClassName("mySlides");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }
-    slides[slideIndex - 1].style.display = "block";
-	slides[slideIndex - 1].classList.add("fade"); 
-    setTimeout(showSlides, 2500); // Change image every 5 seconds
-}
-
+$('.carousel').carousel({
+	interval: 2500
+  })
